47b8a2a3434855ecbb3364dea0ffa5a18167ae08
shivanshu <bishtshivanshu@gmail.com>
2023-04-10 18:36:47 +0530

    added next-auth prisma and added schema generate to postinstall

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import ChatArea from "@/components/chat-area"
+import { SearchDataProvider } from "@/components/search-context"
+import SideSearch from "@/components/side-search"
 import { SiteHeader } from "@/components/site-header"
 
 export default function IndexPage() {
   return (
-    <main className="flex h-screen flex-col justify-between">
-      <SiteHeader />
-      <ChatArea />
-    </main>
+    <div className="flex min-h-screen flex-col">
+      <div className="flex-1 items-start md:grid md:grid-cols-[24rem_minmax(0,1fr)] lg:grid-cols-[24rem_minmax(0,1fr)]">
+        <SearchDataProvider initialData={null}>
+          <aside className="dark:blue-50 fixed top-14 z-30 hidden h-screen shrink-0 overflow-y-auto bg-gradient-to-br from-blue-50 to-purple-50 dark:from-slate-800 dark:via-gray-800 dark:to-slate-900 md:sticky md:flex md:flex-col">
+            <SideSearch />
+          </aside>
+          <main className="flex h-screen flex-col justify-between">
+            <SiteHeader />
+            <ChatArea />
+          </main>
+        </SearchDataProvider>
+      </div>
+    </div>
   )
 }
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
+import { getCurrentUser } from "@/lib/session"
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 import { MainNav } from "@/components/main-nav"
 import { MobileSearch } from "@/components/mobile-search"
 import { ModeToggle } from "@/components/mode-toggle"
 import { buttonVariants } from "@/components/ui/button"
+import { UserAccountNav } from "@/components/user-account-nav"
 
-export function SiteHeader() {
+export async function SiteHeader() {
+  const user = await getCurrentUser()
   return (
     <header className="sticky top-0 z-40 w-full bg-slate-300 shadow-2xl dark:bg-slate-700/60">
       <div className="flex h-16 items-center">
@@ -27,6 +30,22 @@ export function SiteHeader() {
                 <Icons.gitHub className="h-6 w-6" />
               </Link>
               <ModeToggle />
+              {user ? (
+                <UserAccountNav
+                  user={{
+                    name: user.name,
+                    image: user.image,
+                    email: user.email,
+                  }}
+                />
+              ) : (
+                <Link
+                  href="/login"
+                  className={cn(buttonVariants({ size: "sm" }), "px-4")}
+                >
+                  Login
+                </Link>
+              )}
             </nav>
           </div>
         </div>
